9d84b2d88cfcc23fe076c63901c03ed79b16f53c
Alexey Lustin <alustin@silverbulleters.org>
2016-10-16 20:14:58 +0300

    изменил командный интерфейс немного.
    p.s вот как так можно - полностью поменяли формат выгрузки сменив версию

--- tools/decompile.sh
+++ tools/decompile.sh
@@ -4,11 +4,12 @@ stripped=${called_path#[^/]*}
 real_path=`pwd`$stripped
 dir=`dirname $0`
 
-connstring=--ibname /F"./build/ib"
+connstring="--ibname /F./build/ib/"
 USERPWD=
 #connstring=--ibname /F"~/projects/onec/itil"
 #USERPWD=--db-user base --db-pwd 234567890
 export RUNNER_ENV=production
 pushd $dir
-oscript runner.os decompileepf ./build/out/ ./ $connstring $USERPWD
+oscript runner.os decompilecurrent ./src/cf $connstring  $USERPWD
+#oscript runner.os decompileepf ./build/out/ ./ $connstring $USERPWD
 popd
